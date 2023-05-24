3d784a1fd60dac78c943af3c7e266d533dc80c2d
Satvik Madaan <74074890+Satvik1707@users.noreply.github.com>
2023-05-24 22:33:52 +0530

    Update GetInTouch.jsx

--- src/pages/contact/GetInTouch.jsx
+++ src/pages/contact/GetInTouch.jsx
@@ -21,7 +21,7 @@ const handleSubmit = async (e) => {
   };
 
   try {
-    const response = await fetch('https://formspree.io/f/mayzlllw', {
+    const response = await fetch('https://formspree.io/f/mdovvzzg', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
